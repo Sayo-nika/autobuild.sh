a2cabcae6386406594ee9bfda963b9417e1517d3
Capuccino <enra@headbow.stream>
2018-08-05 21:56:32 +0800

    Finalize Release

--- autobuild.sh
+++ autobuild.sh
@@ -1,49 +1,61 @@
 #!/bin/bash
 
+# Copyright 2018(c) The Sayonika Project Authors
+# Licensed under MIT.
+
 echo " ==================================================="
-echo " |  Welcome to Sayonika RenPy DDLC Mod Autobuilder |"
-echo " |                                                 |"
-echo " |                  V0.0.1b                        |"
-echo " |             Licensed under MIT                  |"
+echo "|   Welcome to Sayonika RenPy DDLC Mod Autobuilder  |"
+echo "|                                                   |"
+echo "|                    V0.0.1b                        |"
+echo "|               Licensed under MIT                  |"
 echo " ==================================================="
 echo ""
 echo " GitHub: https://github.com/Sayo-nika/autobuild.sh"
 echo " Bug reports : https://github.com/Sayo-nika/autobuild.sh/issues/new"
 echo ""
 
-read -p "Root folder name: " DIRECTORY
-
-BASE_DIRECTORY=$(echo "$DIRECTORY" | cut -d "/" -f2)
+read -p "Enter your mod's Location: " DIRECTORY
 
 echo "Building Mod in $DIRECTORY"
-echo "If you have this builder script inside your own project folder, make sure you format your Root folder as ../FOLDERNAME."
+echo "If you have this builder script inside your own project folder, make sure you input your folder as ../FOLDERNAME or use '.'."
 echo "Thanks, and have a nice day!"
-if [[ -f "$BASE_DIRECTORY/build/renpy-6.99.12.4-sdk.tar.bz2" ]]; then
-    mkdir -p $BASE_DIRECTORY/build
-    mkdir -p $BASE_DIRECTORY/build/mod
-    cd .. && cp -vRf $BASE_DIRECTORY/* $BASE_DIRECTORY/build/mod
-    cd $BASE_DIRECTORY/build
+
+sleep 3;
+
+if [[ ! -f "$DIRECTORY/build/renpy-6.99.12.4-sdk.tar.bz2" ]]; then
+    mkdir -p $DIRECTORY/build
+    mkdir -p $DIRECTORY/build/mod
+    cp -vRf $DIRECTORY/* $DIRECTORY/build/mod
+    cd $DIRECTORY/build
     tar xf renpy-6.99.12.4-sdk.tar.bz2
     rm renpy-6.99.12.4-sdk.tar.bz2
     mv renpy-6.99.12.4-sdk renpy
     rm -rf renpy-6.99.12.4-sdk
     pull_ddlc_base;
-    echo "{\"version\":\"$(printf "local")\"}" >> version
-    cd renpy 
-    ./renpy.sh "../mod/" lint && ./renpy.sh launcher distribute "../mod/"
+    cd $DIRECTORY/build/renpy 
+    ./renpy.sh "$DIRECTORY/build/mod/" lint && ./renpy.sh launcher distribute "$DIRECTORY/build/mod/"
+    cd ..
 else
     mkdir -p build
-    cd .. && cp -vRf $BASE_DIRECTORY/* $BASE_DIRECTORY/mod
-    cd $BASE_DIRECTORY
+    cp -vRf $DIRECTORY/* $DIRECTORY/mod
+    cd $DIRECTORY
     wget https://www.renpy.org/dl/6.99.12.4/renpy-6.99.12.4-sdk.tar.bz2
     tar xf renpy-6.99.12.4-sdk.tar.bz2
     rm renpy-6.99.12.4-sdk.tar.bz2
     mv renpy-6.99.12.4-sdk renpy
     rm -rf renpy-6.99.12.4-sdk
     pull_ddlc_base;
-    echo "{\"version\":\"$(printf "local")\"}" >> version
-    cd renpy
-    ./renpy.sh "../mod/" lint && ./renpy.sh launcher distribute "../mod/"
+    cd $DIRECTORY/build/renpy
+    ./renpy.sh "$DIRECTORY/build/mod/" lint && ./renpy.sh launcher distribute "$DIRECTORY/build/mod/"
+    cd ..
+fi
+
+if ! [ exit != 0 ]; then
+   echo "Build Successfully made. Find it at $DIRECTORY/build/$DIRECTORY-dists or similar. Happy modding!"
+   exit 0
+else 
+  echo "Uh oh, we can't build your mod in $DIRECTORY. If this is a mistake, file a issue. Thank you."
+  exit 1;
 fi
 
 # This pulls from the Sayonika-maintained S3 Storage for the DDLC base content.
